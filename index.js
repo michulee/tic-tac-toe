6e47cf5b162557b8c55ddd45d5de75ebaa2c998a
Michelle Lee <michellechulee@gmail.com>
2021-03-10 04:27:59 -0800

    notes

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 /**
- * need array for markers to push out
- * if an existing marker, ignore click
+ * all variables
+ * playerOneName, playerOneScore
+ * playerTwoName, playerTwoScore
+ * playerOneMarkers (arr), playerTwoMarkers (arr), gameMarkers (arr) with
+ *  either playerOneName or playerTwoName in arr
+ */
+
+/**
+ * all function
+ * click on gameboard to place marker
+ * click on + to reset all names and scores, and when reset gameMarkers
+ *     also reset playerOneMarkers and playerTwoMarkers
+ */
+
+/**
+* gameboard function
+* playerOneName is X
+* playerTwoName is O
+* whichever player X or O, set all markers to X or O for hover marker
+* when click set index
+*/
+
+/**
+ * winner function
+ * start checking winner when there are 5 gameMarkers
+ * 
+ * OPTION 1
+ * only gameMarkers
+ * 
+ */
+
+/**
+ * how to make modular
+ * start checking at 5, +/- col for hoz, +/- row for vertical, +/- row and col
+ *    for diagonal
+ * if +/- 2 horiz then check col
+ * if +/- 2 vert then check row
+ * if +/- 2 horiz and vert then check diagonal
  */
